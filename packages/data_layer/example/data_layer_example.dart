3637d49aeaf9c277b3d04140ce14d2184fa77dc2
Craig Labenz <craig.labenz@gmail.com>
2025-11-18 15:56:17 -0800

    Initial commit

---
+++ packages/data_layer/example/data_layer_example.dart
@@ -0,0 +1,6 @@
+import 'package:data_layer/data_layer.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/data_layer/test/data_layer_test.dart
@@ -0,0 +1,16 @@
+import 'package:data_layer/data_layer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
